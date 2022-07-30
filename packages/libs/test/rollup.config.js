c5071af27861b0c38b257f649487ebfaf2d828d4
Marcos Gotze <marcos.gotze@atlastechnol.com>
2022-07-30 03:30:48 -0300

    chore: build

---
+++ packages/libs/test/rollup.config.js
@@ -0,0 +1,21 @@
+import vue from 'rollup-plugin-vue'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+
+export default [
+  {
+    input: 'src/main.js',
+    output: [
+      {
+        format: 'esm',
+        file: 'dist/library.mjs'
+      },
+      {
+        format: 'cjs',
+        file: 'dist/library.js'
+      }
+    ],
+    plugins: [
+      vue(), peerDepsExternal()
+    ]
+  }
+]
---
+++ packages/libs/test/src/components.js
@@ -0,0 +1,4 @@
+import Logo from './Logo.vue'
+import Btn from './Btn.vue'
+
+export default { Logo, Btn}
