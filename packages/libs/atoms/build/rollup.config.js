dfad1b85a83e9f4774e50a8f1e76697026889e55
Marcos Gotze <marcos.gotze@atlastechnol.com>
2022-08-01 01:12:12 -0300

    chore: build

--- packages/libs/atoms/build/rollup.config.js
+++ packages/libs/atoms/build/rollup.config.js
@@ -1,14 +1,19 @@
+import babel, { getBabelOutputPlugin } from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs'; // Convert CommonJS modules to ES6
 import vue from 'rollup-plugin-vue'; // Handle .vue SFC files
 import buble from '@rollup/plugin-buble'; // Transpile/polyfill with reasonable browser support
 export default {
     input: 'src/wrapper.js', // Path relative to package.json
-    output: {
-        name: 'atoms',
-        exports: 'named',
-    },
+    output: [
+        {
+          file: 'dist/main.js',
+          format: 'esm',
+          plugins: [getBabelOutputPlugin({ presets: ['@babel/preset-env'] })]
+        }
+      ],
     plugins: [
         commonjs(),
+        babel({ babelHelpers: 'bundled' }),
         vue({
             css: true, // Dynamically inject css as a <style> tag
             compileTemplate: true, // Explicitly convert template to render function
